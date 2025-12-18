c586e88490933dae818bf99cc9395eae976d45a6
Mike <reisner.mic@gmail.com>
2025-12-19 00:16:08 +0100

    Update delphi.js - test ifi dont have the % iframe

--- assets/js/delphi.js
+++ assets/js/delphi.js
@@ -708,7 +708,7 @@ function enableIframeAutoResize(iframe) {
       // If we carry over the previous iframe height, the embedded "dvh" becomes that old height,
       // and the view will never shrink. Pre-reset the iframe height on mode entry to break that loop.
       const minHeightOnModeEntry = Math.floor(
-        window.innerHeight * MIN_IFRAME_VIEWPORT_RATIO
+        window.innerHeight //* MIN_IFRAME_VIEWPORT_RATIO
       );      
       if (mode === "call_mode" || mode === "overview_mode") {
         iframe.style.height = minHeightOnModeEntry + "px";
